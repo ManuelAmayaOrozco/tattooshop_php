6d66f0e033dbe6e5304dfa7e21b115fa7fb0c791
ManuelAmayaOrozco <mamaoro1405@g.educaand.es>
2025-01-29 20:57:50 +0100

    actualización arreglo código

--- README.md
+++ README.md
@@ -1,3 +1,71 @@
 # TATTOO SHOP
 
 Fork este repo
+Fork este repo
+
+## EJERCICIO CLASE
+
+Una vez analizado el código que os suministro vamos a realizar un ejercicio que ponga en práctica la inserción de otros datos en la base de datos y la realización de operaciones de validación sobre dichas inserciones.
+
+### EJERCICIO 1. INSERCIÓN DE TATUADOR EN LA BASE DE DATOS
+Desarrolla una parte de la aplicación que permita dar de alta a nuevos tatuadores en nuestra base de datos. Deberás seguir las siguientes indicaciones:
+
+1. *Tabla de la base de datos*
+La tabla que almacenará a los tatuadores dentro de nuestra base de datos se llamará "**tatuadores**" y contendrá los siguientes campos.
+
+| Campo    | Tipo de Dato          | Tamaño | Descripción                |
+|----------|----------------------|--------|----------------------------|
+| id       | INT AUTO_INCREMENT   | -      | Identificador único       |
+| nombre   | VARCHAR              | 100    | Nombre del usuario        |
+| email    | VARCHAR              | 150    | Correo electrónico único  |
+| password | VARCHAR              | 255    | Contraseña     |
+| foto     | VARCHAR              | 255    | URL de la foto de perfil  |
+| creado_en | TIMESTAMP DEFAULT CURRENT_TIMESTAMP | - | Fecha de creación |
+
+#### Explicación de la Tabla:
+- **`id`**: Identificador único (clave primaria).
+- **`nombre`**: Almacena hasta 100 caracteres.
+- **`email`**: Almacena hasta 150 caracteres y es único.
+- **`password`**: Hasta 255 caracteres (permite almacenar contraseñas)
+- **`foto`**: URL o ruta de la imagen del usuario.
+- **`creado_en`**: Guarda la fecha y hora de creación automáticamente.
+
+2. *Clases PHP y lógica de aplicación*
+Debemos controlar desde el fichero **`index.php`** que cuando llegue una petición a la URI *`/tattooshop_php/tatuadores/alta`* se llame al *Controller* de Tatuadores para que, o bien se muestre el formulario que permita dar de alta un nuevo tatuador, o bien que se realice la inserción del tatuador en la base de datos.
+
+#### Explicación de las peticiones
+- **`/tattooshop_php/tatuadores/alta`** y **`REQUEST_METHOD = GET`**: Se muestra el formulario de alta.
+- **`/tattooshop_php/tatuadores/alta`** y **`REQUEST_METHOD = POST`**: Se realiza el alta del tatuador en la BD.
+
+#### Explicación de la lógica de aplicación
+- **`TatuadorController.php`**: Clase que se encarga de mostrar las vistas relacionadas con Tatuadores y de llamar al `TatuadorModel.php` para interactuar con la base de datos.
+- **`TatuadorModel.php`**: Clase que se encarga de interactuar con la base de datos. Contendrá métodos para insertar, leer, modificar y eliminar registros de la tabla `tatuadores` de la base de datos.
+- **`TatuadorViews`**: Diferentes ficheros que contendrán las vistas relacionadas con Tatuadores.
+    - `TatuadorAltaView.php`: Fichero que contiene el formulario de alta de Tatuadores.
+    - `TatuadorAltaCorrectaView.php`: Fichero que contiene una página para mostrar un mensaje al usuario para indicarle que el alta ha ido bien.
+
+- **Validaciones de los campos**: Comprobar que los campos introducidos en el formulario de alta de tatuadores son correctos:
+    -`id` -> El id se pone automáticamente (auto-incremental), así que no se debe incluir en el formulario.
+    - `nombre`: Comprobar que el campo no venga vacío.
+    - `email`: Comprobar que siga el formato de email correcto.
+    - `password`: Comprobar que el campo no venga vacío.
+    - `foto`: VOSOTRO DECIDÍS CÓMO TRATAR ESTE CAMPO.
+    - `creado_en`: La fecha y la hora no se pide en el formulario, sino que se establece automáticamente.
+Si alguno de los campos del formulario no se validan correctamente, debe mostrarse un mensaje en rojo indicando el error.
+
+
+### EJERCICIO 2. SELECT EN CITAS CON TODOS LOS NOMBRES DE LOS TATUADORES
+Realizar un `<select>` en el formulario de alta citas que muestre todos los nombres de los tatuadores disponibles en la base de datos.
+
+### EJERCICIO 3. PÁGINA INFORMATIVA DE ALTA DE CITA.
+Realizar una página que cuando se dé de alta una cita nueva le aparezca al usuario la siguiente información:
+
+- ``Mensaje``: Mensaje informativo de que se ha dado de alta la cita correctamente.
+- ``Fecha de la cita``: Fecha y hora de la cita seleccionada.
+- ``Descripción de la cita``: Descripción de la cita escrita por el usuario.
+- ``Nombre del cliente``: Nombre del cliente escrito por el usuario.
+- ``Nombre del tatuador``: Nombre del tatuador seleccionado por el usuario.
+- ``Email del tatuador``: Email del tatuador seleccionado por el usuario.
+- ``Foto del tatuador``: Foto del tatuador que ha seleccionado el usuario (este dato lo vais a tener que sacar de la base de datos).
+
+La disposición de los elementos no es importante, lo único importante es que aparezca la información que os indico.
--- models/CitaModel.php
+++ models/CitaModel.php
@@ -1,83 +1,67 @@
 <?php
 
+    require_once "./database/DBHAndler.php";
     class CitaModel {
-
-        private $nombreTabla = "citas";
-        private $conexion;
+        private $nombreTabla = "citas"; // NOMBRE DE LA TABLA DE LA BASE DE DATOS
+        private $conexion;              // ATRIBUTO QUE ALMACENARÁ LA CONEXIÓN A LA BASE DE DATOS
+        private $dbHandler;             // ATRIBUTO QUE ALMACENA LA INSTANCIA DE DBHAndler
 
         public function __construct() {
-            
-        }
-
-
-        public function insertCita($id, $descripcion, $fechaCita, $cliente, $tatuador) {
-
             // CONECTARSE A LA BASE DE DATOS
-            /* 
-            1º NECESITAMOS SABER LOS PARÁMETROS DE CONEXIÓN A LA BASE DE DATOS 
-            - puerto
-            - nombre de la base de datos
+            /*
+            1º NECESITAMOS SABER LOS PARÁMETROS DE CONEXION A LA BASE DE DATOS
+            - IP (localhost o la IP que sea)
             - usuario
             - contraseña
-            - IP (localhost o la IP que sea)
+            - nombre de la base de datos
+            - puerto
+            Inicializamos un objeto DBHandler (el de la clase que hemos construído) que va a ser
+            el encargado de conectar y desconectar la base de datos
             */
-            $hostname = "localhost";
-            $username = "root";
-            $password = "";
-            $bd = "tattoos_bd";
-            $port = "3306";
-            $this->conexion = new mysqli($hostname, $username, $password, $bd, $port); // mysqli es una clase que contiene métodos para interactuar con la BDD
-
-            // COMPROBAR SI LA CONEXIÓN SE HA REALIZADO CORRECTAMENTE
-            if ($this->conexion->connect_error) {
-
-                die("Error de conexión: ". $this->conexion->connect_error);
-
-            }
+            $this->dbHandler = new DBHandler("localhost","root","","tattoos_bd","3306");
+        }
+        /**
+         * MÉTODO PARA INSERTAR UNA CITA EN LA BASE DE DATOS
+         * @param mixed $id
+         * @param mixed $descripcion
+         * @param mixed $fechaCita
+         * @param mixed $cliente
+         * @param mixed $tatuador
+         * @return bool
+         */
+        public function insertCita($id, $descripcion, $fechaCita, $cliente, $tatuador) {
 
             // INSERTAR EN LA BASE DE DATOS
             /*
-            2º UNA SENTENCIA SQL
-            INSERT INTO citas (id, descripcion, fecha_cita, cliente, tatuador) VALUES ($id, $descripcion, $fechaCita, $cliente, $tatuador);
+            2º NECESITAMOS PREPARAR UNA SENTENCIA SQL PARA INSERTAR EN LA BD
+                a) Nos conectamos a la BD
+                b) Escribimos la sentencia SQL -> INSERT INTO citas (id, descripcion, fechaCita, cliente, tatuador) VALUES ($id, $descripcion, $fechaCita, $cliente, $tatuador);
+                c) Realizamos un prepared statement -> Las s corresponden a la posición de la ? y al tipo de dato de la ?
             */
-            $sql = "INSERT INTO $this->nombreTabla (id, descripcion, fecha_cita, cliente, tatuador) VALUES (?, ?, ?, ?, ?);";
-
+            // a) Usamos el método conectar() que hemos hecho para obtener la conexión a la BD
+            $this->conexion = $this->dbHandler->conectar();
+            // b) Escribimos una sentencia SQL tal cual, poniendo ? por cada columna de la tabla de la BD
+            $sql = "INSERT INTO $this->nombreTabla (id, descripcion, fechaCita, cliente, tatuador) VALUES (?, ?, ?, ?, ?)";
+            // c.1) Realizamos un prepared statement con el método .prepare() del objeto $this->conexion
             $stmt = $this->conexion->prepare($sql);
-
-            $stmt->bind_param("sssss", $id, $descripcion, $fechaCita, $cliente, $tatuador); // "Bindear" unir cada parametro a su interrogación. "qué tipo de datos vamos a intercambiar", "los datos en si"
+            // c.2) Intercambiamos las interrogaciones por nuestros valores. Cada s corresponde a una ?, y con la s le decimos que se trata de un string
+            // s -> string, d -> double/float, i -> integer
+            $stmt-> bind_param("sssss", $id, $descripcion, $fechaCita, $cliente, $tatuador); // "bindear"/unir cada parámetro a su interrogación. "qué tipo de datos vamos a intercambiar", "los datos en sí"
 
             /*
             3º EJECUTAR LA SENTENCIA SQL
+            Ejecutamos la query -> .execute(), devuelve true o false si la operación ha sido exitosa o no
+            
+            ¡ATENCIÓN! -> Al conectarnos a la base de datos o al ejecutar la query se pueden producir excepciones
             */
             try {
-
-                if($stmt->execute()) { // EXECUTE DEVUELVE UN TRUE O FALSE -> SI HA SIDO EXITOSA LA OPERACIÓN O NO
-                    return true;
-                } else {
-                    return false;
-                }    
-
+                return $stmt->execute(); // EXECUTE DEVUELVE UN TRUE O FALSE -> SI HA SIDO EXITOSA LA OPERACION O NO
             } catch(Exception $e) {
-
                 return false;
-
             } finally {
-
-                $this->cerrarConexion();
-
+                $this->dbHandler->desconectar(); // USAMOS FINALLY PARA ASEGURARNOS QUE HEMOS CERRADO LA CONEXIÓN A LA BASE DE DATOS
             }
-            
         }
-
-        public function cerrarConexion() {
-
-            /*
-            4º CERRAR LA CONEXIÓN
-            */
-            $this->conexion->close();
-
-        }
-
     }
 
 ?>
